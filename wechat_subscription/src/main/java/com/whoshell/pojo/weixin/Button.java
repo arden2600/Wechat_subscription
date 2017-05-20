911b7097dc73a9c055e5efdb39b3af5c932a2eea
xianSkyKing <xianSky@outlook.com>
2017-05-20 21:23:23 +0800

    公众号菜单按钮基类

---
+++ wechat_subscription/src/main/java/com/whoshell/pojo/weixin/Button.java
@@ -0,0 +1,20 @@
+package com.whoshell.pojo.weixin;
+
+/**
+ * Button : 公众号按钮基类
+ * @author XianSky
+ *
+ */
+public class Button {
+	/*按钮菜单名字*/
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
