4ffcac99eed7c1643b0ec1667910f22ff9958329
xianSkyKing <xianSky@outlook.com>
2017-05-20 21:23:48 +0800

    公众号菜单类

---
+++ wechat_subscription/src/main/java/com/whoshell/pojo/weixin/Menu.java
@@ -0,0 +1,21 @@
+package com.whoshell.pojo.weixin;
+
+/**
+ * Menu : 微信公众号的整个菜单：菜单对象数量以及内部按钮数量的限制参考官网
+ * @author XianSky
+ *
+ */
+public class Menu {
+
+	//菜单中的按钮选项
+	private Button[] buttons;
+
+	public Button[] getButtons() {
+		return buttons;
+	}
+
+	public void setButtons(Button[] buttons) {
+		this.buttons = buttons;
+	}
+
+}
